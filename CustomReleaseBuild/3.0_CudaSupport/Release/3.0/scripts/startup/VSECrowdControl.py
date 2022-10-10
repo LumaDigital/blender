959d1346e36798236772e01fe719125d664770ca
VDiabloV <v.diablo.v@gmail.com>
2022-10-10 15:52:12 +0200

    [VSE] Add button for linked custom property: "Crowd_on".
    - Linked custom properties can't be edited through the editor by default, a script is needed

--- CustomReleaseBuild/3.0_CudaSupport/Release/3.0/scripts/startup/VSECrowdControl.py
+++ CustomReleaseBuild/3.0_CudaSupport/Release/3.0/scripts/startup/VSECrowdControl.py
@@ -2,10 +2,29 @@ import bpy
 
 _CROWD_COLLECTION_NAME_SUBSTRING = "All_crowd"
 
-class Handle_Crowd_Objects(bpy.types.Operator):
+class Crowd_On_Property_Acess(bpy.types.Operator):
+    """The 'Crowd_on' custom property is being referenced therefore can't be edited via the viewport,
+    this operator provides a button to edit the viewport"""
+    bl_idname = "vse.crowd_property_access"
+    bl_label = "VSE Crowd On Operator"
+    
+    def execute(self, context):
+
+        for object in context.selected_objects:
+            if object.get('Crowd_on', None) != None:
+                object['Crowd_on'] = not object['Crowd_on']
+                object.update_tag()
+
+        for area in bpy.context.window.screen.areas:
+            if area.type == 'VIEW_3D':
+                area.tag_redraw()
+                        
+        return {"FINISHED"}
+
+class Crowd_Work_State(bpy.types.Operator):
     """Sets seat object's geometry switch off, and disables seats and stadium in viewport"""
-    bl_idname = "vse.crowds"
-    bl_label = "VSE Crowd Operator"
+    bl_idname = "vse.crowd_work_state"
+    bl_label = "VSE Work State Operator"
     
     _SEATS_COLLECTION_NAME = "Seats"
     _STADIUM_COLLECTION_NAME = "stadium_buildings_COL"
@@ -27,14 +46,14 @@ class Handle_Crowd_Objects(bpy.types.Operator):
                         object.update_tag()
                         object.select_set(True)
                         
-        bpy.data.collections[self._SEATS_COLLECTION_NAME].hide_viewport = bpy.context.scene.vse_crowd_work_state
-        bpy.data.collections[self._STADIUM_COLLECTION_NAME].hide_viewport = bpy.context.scene.vse_crowd_work_state
+        bpy.data.collections[self._SEATS_COLLECTION_NAME].hide_viewport = not bpy.context.scene.vse_crowd_work_state
+        bpy.data.collections[self._STADIUM_COLLECTION_NAME].hide_viewport = not bpy.context.scene.vse_crowd_work_state
                         
         return {"FINISHED"}
 
-class Show_Crowd_Panel(bpy.types.Panel):
-    bl_label = "VSE Crowd Tool"
-    bl_idname = "LD_PT_Crowd"
+class Show_Crowd_WorkState_Panel(bpy.types.Panel):
+    bl_label = "VSE Crowd Work State Tool"
+    bl_idname = "LD_PT_Crowd_WorkState"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_category = 'VSE Crowd'
@@ -48,15 +67,38 @@ class Show_Crowd_Panel(bpy.types.Panel):
         scn = bpy.context.scene
         
         col = layout.column(align=True)
-        col.operator("vse.crowds", text="Toggle work state", icon="TIME")
+        col.operator("vse.crowd_work_state", text="Toggle work state", icon="TIME")
+
+class Show_Crowd_Custom_Property_Panel(bpy.types.Panel):
+    bl_label = "VSE Crowd Tool"
+    bl_idname = "LD_PT_Crowd_CustomProperty"
+    bl_space_type = 'PROPERTIES'
+    bl_region_type = 'WINDOW'
+    bl_category = 'VSE Crowd'
+    
+    @classmethod
+    def poll(self, context):
+        return len(context.selected_objects) > 0 and context.selected_objects[0].get('Crowd_on', None) != None
+    
+    def draw(self, context):
+        layout = self.layout
+        scn = bpy.context.scene
+        
+        col = layout.column(align=True)
+        col.operator("vse.crowd_property_access", text="Toggle Crowd", icon="TIME")
         
 def register():
-    bpy.utils.register_class(Show_Crowd_Panel)
-    bpy.utils.register_class(Handle_Crowd_Objects)
+    bpy.utils.register_class(Show_Crowd_WorkState_Panel)
+    bpy.utils.register_class(Show_Crowd_Custom_Property_Panel)
+    bpy.utils.register_class(Crowd_Work_State)
+    bpy.utils.register_class(Crowd_On_Property_Acess)
+
     
 def unregister():
-    bpy.utils.unregister_class(Show_Crowd_Panel)
-    bpy.utils.unregister_class(Handle_Crowd_Objects)
+    bpy.utils.unregister_class(Show_Crowd_WorkState_Panel)
+    bpy.utils.unregister_class(Show_Crowd_Custom_Property_Panel)
+    bpy.utils.unregister_class(Crowd_Work_State)
+    bpy.utils.unregister_class(Crowd_On_Property_Acess)
     
 # Assign scene value
 bpy.types.Scene.vse_crowd_work_state = bpy.props.BoolProperty(name = "Work State", default=True)
